4d90bee9e77f964aa56d4c8c0e0127ac7b06954c
George Johnson <151577251+GeorgeJohnson04@users.noreply.github.com>
2025-04-03 23:52:17 -0500

    Update Advanced Policy Research Methods Project Replication.R
    Updated code to add PNGS of the tables to create better analysis points

--- Advanced Policy Research Methods Project Replication.R
+++ Advanced Policy Research Methods Project Replication.R
@@ -1,68 +1,79 @@
-#Used Libraries
+# Load Libraries
 library(fixest)
-library(sandwich)
-library(lmtest)
 library(modelsummary)
 library(haven)
+library(gt)
+library(webshot2)
 
-# Load data
-  data_t1 <- "C:/Users/Tommy/OneDrive/Desktop/School/Adv Policy Research Methods/Final Project/AEA2020 T1 DATA.dta"
-  data_t2_1 <- "C:/Users/Tommy/OneDrive/Desktop/School/Adv Policy Research Methods/Final Project/AEA2020 T2 DATA1.dta"
-  data_t2_2 <- "C:/Users/Tommy/OneDrive/Desktop/School/Adv Policy Research Methods/Final Project/AEA2020 T2 DATA2.dta"
+# Load Data
+data_t1 <- haven::read_dta("C:/Users/Tommy/OneDrive/Desktop/School/Old Classes /Adv Policy Research Methods/Final Project/AEA2020 T1 DATA.dta")
+data_t2_1 <- haven::read_dta("C:/Users/Tommy/OneDrive/Desktop/School/Old Classes/Adv Policy Research Methods/Final Project/AEA2020 T2 DATA1.dta")
+data_t2_2 <- haven::read_dta("C:/Users/Tommy/OneDrive/Desktop/School/Old Classes/Adv Policy Research Methods/Final Project/AEA2020 T2 DATA2.dta")
 
-  data_t1 <- haven::read_dta(data_t1)
-  data_t2_1 <- haven::read_dta(data_t2_1)
-  data_t2_2 <- haven::read_dta(data_t2_2)
-
-# Table 5 Rep
-  # Model 1
-    model1 <- feols(crime ~ job + det_befTr + age10 + yschool + male + english + timeus + married + less1y | state_cap + state_f + year, 
+# Table 5 Models
+model1 <- feols(crime ~ job + det_befTr + age10 + yschool + male + english + timeus + married + less1y | state_cap + state_f + year, 
                 weights = ~weights, data = data_t2_1[data_t2_1$less1y == 1, ], cluster = ~state_cap)
 
-  # Model 2
-    model2 <- feols(crime ~ jobd1 + jobd2 + jobd3 + d1 + d2 + d3 + det_befTr + age10 + yschool + male + married + english | state_cap + state_f + year, 
+model2 <- feols(crime ~ jobd1 + jobd2 + jobd3 + d1 + d2 + d3 + det_befTr + age10 + yschool + male + married + english | state_cap + state_f + year, 
                 weights = ~weights, data = data_t2_1[data_t2_1$less1y == 1, ], cluster = ~state_cap)
 
-  # Model 3
-    model3 <- feols(timedet ~ job + det_befTr + age10 + yschool + male + english + timeus + married + less1y | state_cap + state_f + year, 
+model3 <- feols(timedet ~ job + det_befTr + age10 + yschool + male + english + timeus + married + less1y | state_cap + state_f + year, 
                 weights = ~weights, data = data_t2_1[data_t2_1$less1y == 1, ], cluster = ~state_cap)
 
-  # Model 4
-    model4 <- feols(timedet ~ jobd1 + jobd2 + jobd3 + d1 + d2 + d3 + det_befTr + age10 + yschool + male + married + english | state_cap + state_f + year, 
+model4 <- feols(timedet ~ jobd1 + jobd2 + jobd3 + d1 + d2 + d3 + det_befTr + age10 + yschool + male + married + english | state_cap + state_f + year, 
                 weights = ~weights, data = data_t2_1[data_t2_1$less1y == 1, ], cluster = ~state_cap)
 
-  # Results Table 5
-    print(etable(
-      "Model 1" = model1,
-      "Model 2" = model2,
-      "Model 3" = model3,
-      "Model 4" = model4
-                        ))
+# Create Styled Table 5
+table5 <- modelsummary(
+  list("Model 1 (Crime)" = model1, "Model 2 (Crime)" = model2, "Model 3 (Detention)" = model3, "Model 4 (Detention)" = model4),
+  output = "gt",
+  stars = TRUE,
+  gof_omit = 'IC|Log|F|Adj|RMSE',
+  fmt = 3
+) %>%
+  tab_header(title = "📊 Table 5: Regression Results - Less than 1 Year in U.S.") %>%
+  tab_style(style = list(cell_fill(color = "#f0f4f8")), locations = cells_body()) %>%
+  tab_options(
+    table.font.size = px(13),
+    data_row.padding = px(2),
+    heading.title.font.size = px(16),
+    column_labels.font.weight = "bold"
+  )
 
-# Table 6 Rep
-  # Model 5
-    model5 <- feols(crime ~ job + timeusa + age10 + yschool + male + english + married | state_cap + state_f + year, 
+# Save Table 5
+gtsave(table5, "Table_5_Results.png", zoom = 1.25)
+
+# Table 6 Models
+model5 <- feols(crime ~ job + timeusa + age10 + yschool + male + english + married | state_cap + state_f + year, 
                 weights = ~weights, data = data_t2_2, cluster = ~state_cap)
 
-  # Model 6
-    model6 <- feols(crime ~ jobd4 + jobd5 + jobd6 + jobd7 + d4 + d5 + d6 + age10 + yschool + male + married + english | state_cap + state_f + year, 
+model6 <- feols(crime ~ jobd4 + jobd5 + jobd6 + jobd7 + d4 + d5 + d6 + age10 + yschool + male + married + english | state_cap + state_f + year, 
                 weights = ~weights, data = data_t2_2, cluster = ~state_cap)
-  # Model 7 
-    model7 <- feols(timedet ~ job + timeusa + age10 + yschool + male + english + married | state_cap + state_f + year, 
-                    weights = ~weights, data = data_t2_2, cluster = ~state_cap)
-    
-  # Model 8 
-    model8 <- feols(timedet ~ jobd4 + jobd5 + jobd6 + jobd7 + d4 + d5 + d6 + age10 + yschool + male + married + english | state_cap + state_f + year, 
-                    weights = ~weights, data = data_t2_2, cluster = ~state_cap)
-    
 
-  # Results Table 6 
-    print(etable(
-      "Model 5" = model5,
-      "Model 6" = model6,
-      "Model 7 (Months Detained)" = model7,
-      "Model 8 (Months Detained)" = model8
-                        ))
-  
-    
+model7 <- feols(timedet ~ job + timeusa + age10 + yschool + male + english + married | state_cap + state_f + year, 
+                weights = ~weights, data = data_t2_2, cluster = ~state_cap)
+
+model8 <- feols(timedet ~ jobd4 + jobd5 + jobd6 + jobd7 + d4 + d5 + d6 + age10 + yschool + male + married + english | state_cap + state_f + year, 
+                weights = ~weights, data = data_t2_2, cluster = ~state_cap)
+
+# Create Styled Table 6
+table6 <- modelsummary(
+  list("Model 5 (Crime)" = model5, "Model 6 (Crime)" = model6, "Model 7 (Detention)" = model7, "Model 8 (Detention)" = model8),
+  output = "gt",
+  stars = TRUE,
+  gof_omit = 'IC|Log|F|Adj|RMSE',
+  fmt = 3
+) %>%
+  tab_header(title = "📊 Table 6: Regression Results - More than 1 Year in U.S.") %>%
+  tab_style(style = list(cell_fill(color = "#f0f4f8")), locations = cells_body()) %>%
+  tab_options(
+    table.font.size = px(13),
+    data_row.padding = px(2),
+    heading.title.font.size = px(16),
+    column_labels.font.weight = "bold"
+  )
+
+# Save Table 6
+gtsave(table6, "Table_6_Results.png", zoom = 1.25)
+
     
